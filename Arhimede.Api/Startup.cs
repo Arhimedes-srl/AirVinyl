98acce148b4a3dc0f34cb66728e6ab1c602ed453
Moisei Mihai <misu@arhimedes.ro>
2021-11-25 13:48:03 +0200

    Add project files.

---
+++ Arhimede.Api/Startup.cs
@@ -0,0 +1,72 @@
+using AirVinyl.API.DbContexts;
+using AirVinyl.EntityDataModels;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OData.Edm;
+
+
+namespace AirVinyl
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<AirVinylDbContext>(options =>
+          options.UseSqlServer(
+              Configuration.GetConnectionString("DefaultConnection"))
+          .EnableSensitiveDataLogging());
+
+            services.AddControllers().AddOData(options =>
+             options.AddRouteComponents("odata", new AirVinylEntityDataModel().GetEntityDataModel())
+                    .Select()
+                    .Expand()
+                    .OrderBy()
+                    .SetMaxTop(10)
+                    .Count()
+                    .Filter());
+
+
+          
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseODataRouteDebug();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+        }
+
+
+  
+    }
+}
